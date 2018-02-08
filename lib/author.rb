566f5c84437f66e41861a4cfefee6abb97936c41
Sean Oughton <seanoughton@gmail.com>
2018-02-08 18:06:33 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
 
   attr_accessor :name, :posts
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
@@ -9,12 +11,14 @@ class Author
 
   def add_post(post)
     @posts << post
+    @@post_count += 1
     post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
+    @@post_count += 1
     post.author = self
   end
 
